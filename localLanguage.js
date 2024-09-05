171682a3be4c508c3f0c269a63c580802a5f2ec1
Zehua Liu <u7544341@anu.edu.au>
2024-09-05 16:59:59 +1000

    commit

--- localLanguage.js
+++ localLanguage.js
@@ -3,27 +3,27 @@ function switchLanguage(language) {
         // element.innerText = element.getAttribute('data-' + language);
 
     if (element.tagName.toLowerCase() === 'input' && element.type === 'submit') {
-        element.value = element.getAttribute('data-' + language); // 更新 value 属性
+        element.value = element.getAttribute('data-' + language);
     } else {
-        element.innerText = element.getAttribute('data-' + language); // 更新 innerText
+        element.innerText = element.getAttribute('data-' + language); 
     }
 });
 
     localStorage.setItem('language', language);
     if (language == 'cn') {
-        document.getElementById('logo-nihonga').style.top = '70%'
-        document.getElementById('logo-crayon').style.top = '70%'
-        document.getElementById('logo-caricature').style.top = '70%'
-        document.getElementById('logo-about').style.top = '70%'
+        // document.getElementById('logo-nihonga').style.top = '70%'
+        // document.getElementById('logo-crayon').style.top = '70%'
+        // document.getElementById('logo-caricature').style.top = '70%'
+        // document.getElementById('logo-about').style.top = '70%'
         document.body.style.fontFamily = '"Noto Serif", serif';
         // document.getElementById('header').style.fontFamily
         // document.getElementById('contact-title').style.fontSize = '25px';
     }
     else {
-        document.getElementById('logo-nihonga').style.top = '50%'
-        document.getElementById('logo-crayon').style.top = '50%'
-        document.getElementById('logo-caricature').style.top = '50%'
-        document.getElementById('logo-about').style.top = '50%'
+        // document.getElementById('logo-nihonga').style.top = '50%'
+        // document.getElementById('logo-crayon').style.top = '50%'
+        // document.getElementById('logo-caricature').style.top = '50%'
+        // document.getElementById('logo-about').style.top = '50%'
         document.body.style.fontFamily = '"Arsenal SC", sans-serif';
     }
 }
